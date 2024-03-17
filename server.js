bc1bf090142e3cf49456c47fa7e5738ad930458f
Geetika Valecha <geetikavalecha@Geetikas-Air.lan>
2024-03-17 18:51:26 +0530

    update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: "geetikav0101@gmail.com",
-    pass: "add pass here"
+    pass: "qwkl mvwy jrse djbd"
   },
 });
 
